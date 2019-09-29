d98d2328f6ad40ed0e8eb782a3274f30fce89a0a
vianaernesto <le.viana@uniandes.edu.co>
2019-09-29 13:18:08 -0500

    Servidor Requerimiento
    1,2 (falta poner archivos grandes),3 (falta el hash),6,7

--- app/app.py
+++ app/app.py
@@ -1,34 +1,90 @@
 import socket
+import hashlib
+import _thread
+import sys
+import time
 
-
+def nuevoCliente(socketCliente,addr, archivo, cliente):
+    while True:
+        socketCliente.settimeout(1)
+        try:
+            ##En este se recibe el primer saludo de un cliente
+            ##Si el cliente no cumple el protocolo se termina la conexion.
+            print("Se recibe al cliente: ", cliente)
+            saludo = socketCliente.recv(1024)
+            if(saludo == b"syn"):
+                print("El cliente se ha conectado correctamente")
+                socketCliente.send(b"ack")
+            else:
+                print("El cliente no cumple el protocolo")
+                socketCliente.shutdown(socket.SHUT_WR)
+        except Exception as e1:
+            print ('El cliente no envio saludo y se cumplio el tiempo de conexion.')
+            break
+        socketCliente.settimeout(1)
+        try:
+            ##En este se recibe el mensaje de preparacion del cliente.
+            solicitud = socketCliente.recv(1024)
+            if(solicitud == b"Preparado"):
+                print("El cliente esta preparado para recibir el archivo.")
+                print("Enviando archivo")
+                with open(archivo, 'rb') as fa:
+                    print('Archivo preparado')
+                    fa.seek(0,0)
+                    t1 = time.time()*1000
+                    while True:
+                        data = fa.read(1024) 
+                        socketCliente.send(data)
+                        if not data:
+                            t2 = time.time()*1000
+                            break
+                    fa.close()
+        except Exception as e2:
+            print ('El cliente no envio mensaje de preparacion para recibir archivo y se cumplio el tiempo de conexion.')
+            break
+        socketCliente.settimeout(20)
+        try: 
+            print("Se recibe mensaje de confirmacion:")
+            confirmacion = socketCliente.recv(1024)
+            if(confirmacion == b"Recibido"):
+                print("Archivo enviado correctamente")
+            break
+        except Exception as e3:
+            print ('El cliente no envio confirmacion y se cumplio el tiempo de conexion.')
+            break
+    tiempoTransferencia = t2-t1
+    socketCliente.shutdown(socket.SHUT_WR)
+    socketCliente.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(),5000))
-s.listen(1)
+s.listen(25)
 
-archivo1 = 'tux.jpg'
-archivo2 = 'mario.png'
-archivo = ''
+archivo1 = 'archivo1.png'
+archivo2 = 'archivo2.png'
+archivo = ""
 
-while True:
-    (conn, address) = s.accept()
-    solicitud = conn.recv(1024)
-    if(solicitud == b"archivo1"):
+
+print('Servidor Iniciado')
+escoger = True
+while(escoger):
+    escoger=False
+    print('Escoger archivo a enviar: 1: archivo de 100 Mb y 2: archivo de 250Mb')
+    escogido = input()
+    if(escogido == "1"):
         archivo = archivo1
-    else:
+    elif(escogido == "2"):
         archivo = archivo2
-    
-    print('Abriendo archivo',archivo)
-    with open(archivo, 'rb') as fa:
-        print('se abrio el archivo')
-        fa.seek(0,0)
-        print("enviando archivo.")
-        while True:
-            data = fa.read(1024)
-            conn.send(data)
-            if not data:
-                break
-        fa.close()
-        print("Archivo enviado")
-    break
+    else:
+        escoger=True
+        print('No digito un numero valido, vuelva a intentarlo')
+
+print('Escuchando a clientes')
+numClientes = 0
+while numClientes < 26:
+    (conn, address) = s.accept()
+    numClientes += 1
+    _thread.start_new_thread(nuevoCliente,(conn,address,archivo,numClientes))
+s.shutdown(socket.SHUT_WR)
 s.close()
+sys.exit(0)
